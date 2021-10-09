0db3ece0156fda325856d3439d5fbebb4fb83144
Kaspar Martin Suursalu <kasparsu@gmail.com>
2021-10-09 23:14:35 +0300

    add localized Text class and tests

--- bin/run.php
+++ bin/run.php
@@ -1,7 +1,10 @@
 <?php
 //This is example of usage and should be kept as is.
 
+use Faker\Provider\et_EE\Text;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 $faker = Faker\Factory::create('et_EE');
-echo $faker->name;
+$faker->addProvider(new Text($faker));
+echo $faker->realText();
